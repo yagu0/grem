15db35580450c284e8f236d45d4608b15f346ab0
Benjamin Auder <benjamin.auder@somewhere>
2025-11-14 16:25:17 +0100

    Update debug.c

--- c_project/bin/debug.c
+++ c_project/bin/debug.c
@@ -1,4 +1,5 @@
 #include "../src/graph.h"
+#include "../src/spring_embed.h"
 #include <stdio.h>
 
 // Compile with :
@@ -7,7 +8,8 @@
 int main(int argc, char** argv) {
   //Graph g = make_random_tree(10, 0, 100, 32);
   //Graph g = make_random_graph(10, 0.2, 100, 32);
-  Graph g = make_random_binary_tree(10, 100, 32);
+  Graph g = make_random_binary_tree(100, 100, 32);
+  spring_layout(&g, 100, 100);
   free_graph(g);
   return 0;
 }
